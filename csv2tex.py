cf2af2f8ccc443eb54930aafff53b8e29723f103
Lucas Treffenstädt <lucas@treffenstaedt.de>
2015-07-13 10:04:27 +0200

    Changed default output behaviour to print to stdout

--- csv2tex.py
+++ csv2tex.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import configparser as cp
-from sys import argv,exit
+from sys import argv,exit,stdout,stderr
 from numpy import genfromtxt
 from math import log,floor
 from getopt import gnu_getopt
@@ -69,12 +69,20 @@ def getErrorScale(config, section,scale):
     else:
         return scale
 
+def printSmart(string, outfile):
+    if(outfile==None):
+        print(string,file=stderr)
+    else:
+        print(string)
+
 ###########################################################
+
+usage="Usage: csv2tex [OPTIONS]\n  -i file, --in=file\n    read data from file\n    default: in.dat\n\n  -o file, --out=file\n    write table to file\n    default: write to stdout\n\n  -c file, --conf=file\n    read configuration from file\n    default: csv2tex.conf"
 
 options,arguments=gnu_getopt(argv[1:],"i:c:o:h",["in=","out=","conf=","help"])
 options=dict(options)
 if("--help" in options or "-h" in options):
-    print("Usage: csv2tex [OPTIONS]\n  -i file, --in=file\n    read data from file\n    default: in.dat\n\n  -o file, --out=file\n    write table to file\n    default: out.tex\n\n  -c file, --conf=file\n    read configuration from file\n    default: csv2tex.conf")
+    print(usage)
     exit(0)
 if("--in" in options):
     infile=options["--in"]
@@ -87,7 +95,7 @@ if("--out" in options):
 elif("-o" in options.keys()):
     outfile=options["-o"]
 else:
-    outfile="out.tex"
+    outfile=None
 if("--conf" in options):
     conffile=options["--conf"]
 elif("-c" in options.keys()):
@@ -95,22 +103,22 @@ elif("-c" in options.keys()):
 else:
     conffile="csv2tex.conf"
 if(arguments!=[]):
-    print("Unrecognized Options: "+str(arguments))
-    print("Usage: csv2tex [OPTIONS]\n  -i file, --in=file\n    read data from file\n    default: in.dat\n\n  -o file, --out=file\n    write table to file\n    default: out.tex\n\n  -c file, --conf=file\n    read configuration from file\n    default: csv2tex.conf")
+    printSmart("Unrecognized Options: "+str(arguments)),outfile
+    printSmart(usage,outfile)
     exit(1)
 
-print("reading configuration file "+conffile)
+printSmart("reading configuration file "+conffile,outfile)
 config=cp.ConfigParser(interpolation=None)
 cfg=config.read(conffile)
 if(cfg==[]):
-    print("    Error: Configuration file not found")
+    printSmart("    Error: Configuration file not found",outfile)
     exit(1)
 
-print("reading data file "+infile)
+printSmart("reading data file "+infile,outfile)
 try:
     data = genfromtxt(infile)
 except OSError:
-    print("    Error: data file not found or unreadable")
+    printSmart("    Error: data file not found or unreadable",outfile)
     exit(1)
 
 formatierung={}
@@ -141,40 +149,48 @@ for section in config.sections():
     else:
         formatierung[section]+="$"
 
-print("writing to output file "+outfile)
+if(outfile==None):
+    print("writing to stdout",file=stderr)
+else:
+    print("writing to output file "+outfile)
 try:
-    with open(outfile, "w") as output:
-        output.write("\\begin{{tabular}}{{{align:s}}}\n".format(align=align))
-        output.write("\\toprule\n")
-        output.write(header+"\\\\\n")
-        output.write("\\midrule\n")
-        for line in range(len(next(iter(daten.values())))):
-            zeile=""
-            for section in config.sections():
-                if(zeile!=""):
-                    zeile+="&"
-                form=formatierung[section]
-                x=daten[section][line]
-                if section in fehler:
-                    s=fehler[section][line]
-                    if(precision[section]==None):
-                        prec=-floor(log(s)/log(10))
-                    else:
-                        prec=precision[section]
-                    s=round(s,prec)
-                    x=round(x,prec)
-                    zeile+=form.format(precision=max(prec,0),x=x,precision_s=max(prec,0),s=s)
+    if(outfile==None):
+        output=stdout
+    else:
+        output=open(outfile, "w")
+    print("\\begin{{tabular}}{{{align:s}}}".format(align=align),file=output)
+    print("\\toprule",file=output)
+    print(header+"\\\\",file=output)
+    print("\\midrule",file=output)
+    for line in range(len(next(iter(daten.values())))):
+        zeile=""
+        for section in config.sections():
+            if(zeile!=""):
+                zeile+="&"
+            form=formatierung[section]
+            x=daten[section][line]
+            if section in fehler:
+                s=fehler[section][line]
+                if(precision[section]==None):
+                    prec=-floor(log(s)/log(10))
                 else:
-                    if(precision[section]==None):
-                        prec=0
-                    else:
-                        prec=precision[section]
-                    zeile+=form.format(precision=prec,x=x)
-            zeile+="\\\\\n"
-            output.write(zeile)
-        output.write("\\bottomrule\n")
-        output.write("\\end{tabular}\n")
+                    prec=precision[section]
+                s=round(s,prec)
+                x=round(x,prec)
+                zeile+=form.format(precision=max(prec,0),x=x,precision_s=max(prec,0),s=s)
+            else:
+                if(precision[section]==None):
+                    prec=0
+                else:
+                    prec=precision[section]
+                zeile+=form.format(precision=prec,x=x)
+        zeile+="\\\\"
+        print(zeile,file=output)
+    print("\\bottomrule",file=output)
+    print("\\end{tabular}",file=output)
+    if(outfile!=None):
+        output.close()
 except Exception:
-    print("  writing failed")
+    printSmart("  writing failed",outfile)
     exit(1)
-print("    done")
+printSmart("    done",outfile)
